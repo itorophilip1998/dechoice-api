68834e9140f2800e03f6f5f47f414d7bcce15eb1
itphilip1998 <itkeyzzpianny@gmail.com>
2022-03-12 12:46:35 +0100

    dev

--- routes/web.js
+++ routes/web.js
@@ -2,6 +2,6 @@ const express = require("express");
 const route = express.Router();
 
 route
-  .get('/', (req, res) => res.send('Api'));
+  .get('/', (req, res) => res.send('Welcome to the server'));
 
 module.exports = route;
